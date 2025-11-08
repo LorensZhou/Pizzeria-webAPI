b0955990c22eb2d0b836568c16c5723d21f22ed0
LorensZhou <lorenszhou@yahoo.com>
2025-11-08 21:45:04 +0100

    Initial commit

---
+++ src/main/java/nl/novi/pizzeria_webAPI/PizzeriaWebApiApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.pizzeria_webAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/pizzeria_webAPI/PizzeriaWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.pizzeria_webAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
